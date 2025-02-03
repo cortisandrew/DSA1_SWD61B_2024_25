748e1bb3bb0ad413042e92c1ac58b530ec02c19a
andrewc <andrew.cortis@mcast.edu.mt>
2025-02-03 10:27:40 +0100

    Added code examples of ref and value types

--- Ref_vs_Value_Types/Program.cs
+++ Ref_vs_Value_Types/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Ref_vs_Value_Types;
+
+// Value types
+Console.WriteLine("Value types:");
+
+int a = 5;
+int b = 6;
+
+Console.WriteLine($"The value of a = {a}");
+Console.WriteLine($"The value of b = {b}");
+
+// Assign a the value of b
+a = b;
+
+Console.WriteLine("a = b");
+Console.WriteLine($"The value of a = {a}");
+Console.WriteLine($"The value of b = {b}");
+
+b = 10;
+Console.WriteLine("b=10");
+Console.WriteLine($"The value of a = {a}");
+Console.WriteLine($"The value of b = {b}");
+
+Console.WriteLine();
+Console.WriteLine("Reference Types:");
+
+ReferenceTypeData c = new ReferenceTypeData(5);
+ReferenceTypeData d = new ReferenceTypeData(6);
+
+Console.WriteLine($"The value of c = {c.InstanceValue}");
+Console.WriteLine($"The value of d = {d.InstanceValue}");
+
+c = d;
+
+Console.WriteLine("c=d");
+Console.WriteLine($"The value of c = {c.InstanceValue}");
+Console.WriteLine($"The value of d = {d.InstanceValue}");
+
+d.InstanceValue = 10;
+Console.WriteLine("d=10");
+Console.WriteLine($"The value of c = {c.InstanceValue}");
+Console.WriteLine($"The value of d = {d.InstanceValue}");
+
+
+
---
+++ Ref_vs_Value_Types/ReferenceTypeData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ref_vs_Value_Types
+{
+    public class ReferenceTypeData
+    {
+        public int InstanceValue { get; set; }
+
+        public ReferenceTypeData() { }
+
+        public ReferenceTypeData(int instanceValue) {  this.InstanceValue = instanceValue; }
+    }
+}
