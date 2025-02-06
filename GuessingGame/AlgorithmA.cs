12ae0e7031fdd8b24549f6cb333f61dc1c71e155
Andrew Cortis <andrew.cortis@mcast.edu.mt>
2025-02-06 09:59:06 +0100

    Started code for guessing game

---
+++ GuessingGame/AlgorithmA.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    /// <summary>
+    /// Take a secret number
+    /// Find the maximum possible secret number
+    /// Start guessing 1, 2, 3, ...
+    /// Until we find the secret number
+    /// </summary>
+    internal class AlgorithmA
+    {
+        private SecretNumber secretNumber;
+        private int totalGuesses = 0;
+
+        internal AlgorithmA(SecretNumber s)
+        {
+            this.secretNumber = s;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>(secretNumber, number of Guesses)</returns>
+        internal (int, int) GuessNumber() {
+            totalGuesses = 0;
+
+            for (int i = 1; i <= secretNumber.MaxNumber(); i++)
+            {
+                // make a guess
+                totalGuesses++;
+
+                if (secretNumber.Guess(i) == 0)
+                {
+                    // we found the correct number!
+                    return (i, totalGuesses);
+                }
+                // we did not guess correctly, keep guessing
+            }
+
+            // the secret number is not between 1 and max number (inclusive)
+            // there is an error!
+            throw new ApplicationException("There is an issue somewhere!");
+        }
+
+    }
+}
---
+++ GuessingGame/SecretNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    internal class SecretNumber
+    {
+        int secretNumber;
+        int maxNumber;
+
+        /// <summary>
+        /// If max Number is 100, secret number is between 1 and 100 (both inclusive)
+        /// </summary>
+        /// <param name="maxNumber"></param>
+        internal SecretNumber(int maxNumber)
+        {
+            Random r = new Random();
+            secretNumber = r.Next(1, maxNumber + 1);
+            this.maxNumber = maxNumber;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="guess"></param>
+        /// <returns>0 if you guessed correctly
+        /// 1 if guess is larger than secret number
+        /// -1 if guess is smaller than secret number</returns>
+        internal int Guess(int guess)
+        {
+            return guess.CompareTo(secretNumber);
+        }
+
+        /// <summary>
+        /// What is the maximum number to guess?
+        /// </summary>
+        /// <returns></returns>
+        internal int MaxNumber() { return maxNumber; }
+    }
+}
