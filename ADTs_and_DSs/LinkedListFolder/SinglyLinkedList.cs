84c99ddcf47e763f02feeb79b97aec1921eed809
Andrew Cortis <andrew.cortis@mcast.edu.mt>
2025-03-06 09:30:54 +0100

    Created the structure and operations of the Singly Linked List

---
+++ ADTs_and_DSs/LinkedListFolder/SinglyLinkedList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADTs_and_DSs.LinkedListFolder
+{
+    /// <summary>
+    /// A singly linked list containing elements of type T
+    /// </summary>
+    /// <typeparam name="T">The type of element we want to store</typeparam>
+    public class SinglyLinkedList<T>
+    {
+        /// <summary>
+        /// The head of list points towards the instance of type SLL Node that stores
+        /// the first element of the list
+        /// If there are no elements, the head of list will point towards null (i.e. there is no node and no elements being stored)
+        /// </summary>
+        private SinglyLinkedListNode<T>? head = null;
+
+        public SinglyLinkedListNode<T>? Head
+        {
+            get { return head; }
+        }
+
+        /*
+        /// <summary>
+        /// The tail of list points towards the last SLL Node in the list, which stores
+        /// the last element of the list (if it exists)
+        /// If the list is empty, no elements exist and this points towards null
+        /// </summary>
+        /// <remarks>I will need this and add the information later...</remarks>
+         private SinglyLinkedListNode<T>? tail = null;
+        */
+
+        /// <summary>
+        /// Keeps track of the number of elements stored within the linked list
+        /// </summary>
+        private int count = 0;
+
+        public int Count() { return count; }
+
+        public bool IsEmpty() {
+            return count == 0;
+            //return head == null;
+        }
+
+        public void InsertFirst(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertLast(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T RemoveFirst() { throw new NotImplementedException(); }
+
+        public T RemoveLast() {  throw new NotImplementedException(); }
+
+
+        public void InsertAfterNode(SinglyLinkedListNode<T> cursor, T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T RemoveAfterNode(SinglyLinkedListNode<T> cursor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertBeforeNode(SinglyLinkedListNode<T> cursor, T element)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
---
+++ ADTs_and_DSs/LinkedListFolder/SinglyLinkedListNode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADTs_and_DSs.LinkedListFolder
+{
+    /// <summary>
+    /// Represents a single node in the linked list
+    /// Stores a single element of type T
+    /// (Does something else as well)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class SinglyLinkedListNode<T>
+    {
+        /// <summary>
+        /// The element which is being stored within this node
+        /// </summary>
+        private T element;
+
+        public T Element
+        {
+            get { return element; }
+            internal set { element = value; }
+        }
+
+        /// <summary>
+        /// A pointer/reference to the next node in the linked list
+        /// If this is the tail of list, the next node does not exist!
+        /// In this case, we use a null
+        /// </summary>
+        private SinglyLinkedListNode<T>? next = default;
+
+        public SinglyLinkedListNode<T>? Next
+        {
+            get { return next; }
+            internal set { next = value; }
+        }
+
+        public SinglyLinkedListNode(T element)
+        {
+            this.element = element;
+        }
+
+        public SinglyLinkedListNode(T element, SinglyLinkedListNode<T>? next) : this(element)
+        {
+            this.next = next;
+        }
+    }
+}
