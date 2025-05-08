f6a10d05fe14646a64bb4b7964b5452dbf63fd7f
Andrew Cortis <andrew.cortis@mcast.edu.mt>
2025-05-08 09:55:46 +0200

    Added a simple LCG.

---
+++ PRNG/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PRNG;
+
+Console.WriteLine("Hello, World!");
+
+LCG myLCG = new LCG();
+
+for (int i = 0; i < 50; i++)
+{
+    Console.WriteLine(myLCG.Next(1,7));
+}
